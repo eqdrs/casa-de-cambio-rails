829df45f063ef63d7a549c9eb0fa880c0189b227
Everton Quadros <evertonquadros15@gmail.com>
2018-12-22 01:38:09 -0300

    Administrador edita um cliente

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users =User.all
+  end
+
   def new
     @user = User.new
   end
@@ -10,6 +14,21 @@ class UsersController < ApplicationController
     else
       flash.now[:warning] = 'Você deve informar todos os dados do usuário'
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else   
+      flash.now[:warning] = 'Você deve informar todos os dados do usuário'
+      render :edit
     end
   end
 
