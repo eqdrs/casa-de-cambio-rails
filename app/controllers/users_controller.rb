428b1ec36cb5b903a1b5c5780fcbb7a573b4ec7b
Everton Quadros <evertonquadros15@gmail.com>
2018-12-24 21:12:36 -0300

    Adicionando busca de usuário por nome.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users =User.all
+    if params[:name]
+      @users = User.where("name like ?", "%#{params[:name]}%")
+    else
+      @users =User.all
+    end
   end
 
   def new
